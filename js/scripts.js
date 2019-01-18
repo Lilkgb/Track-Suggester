ec27ec118b50405dced78a989e35911a2a64700c
lilkgb <mcleanrs@comcast.net>
2019-01-18 13:26:35 -0800

    add submit function for hide survey when submit click. & show survey when show survey button click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+var name = $(".name").val();
+
 $(document).ready(function() {
-  
-})
+  $("#survey").submit(function(event) {
+    $("#survey").hide("slow");
+
+    event.preventDefault();
+  });
+  $("#showSurvey").click(function() {
+    $("#survey").show("slow");
+  });
+});
