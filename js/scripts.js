179a30b1fe4ae2c07d4557bafcf2a12c42d8df21
lilkgb <mcleanrs@comcast.net>
2019-01-18 14:15:28 -0800

    add var for experience new, little, intermediate and expert. use for later for response

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 var name = $(".name").val();
+var new = $("input:radio[name=new]:checked").val();
+var little = $("input:radio[name=little]:checked").val();
+var intermediate = $("input:radio[name=intermediate]:checked").val();
+var expert = $("input:radio[name=expert]:checked").val();
 
 $(document).ready(function() {
   $("#survey").submit(function(event) {
