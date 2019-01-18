06c9555d394e9312f27316d3f46c93a77cfba694
lilkgb <mcleanrs@comcast.net>
2019-01-18 15:10:04 -0800

    working on if else statments for radio buttons. nothing is working at the moment'

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 var name = $(".name").val();
-var new = $("input:radio[name=new]:checked").val();
-var little = $("input:radio[name=little]:checked").val();
-var intermediate = $("input:radio[name=intermediate]:checked").val();
-var expert = $("input:radio[name=expert]:checked").val();
+var ne = document.getElementById("#new");
+var little = $("input:radio[name=experience]:checked").val();
+var intermediate = $("input:radio[name=experience]:checked").val();
+var expert = $("input:radio[name=experience]:checked").val();
 
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     $(".jumbotron").hide("slow");
     $("#showSurvey").show();
+    if (ne.checked) {
+      alert("you are new to this");
+    }
+    else if (".2".checked) {
+      alert("you can learn");
+    }
+    else {
+      alert("error");
+    }
 
     event.preventDefault();
   });
